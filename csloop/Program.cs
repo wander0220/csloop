d134e5c18c934eca19072dafc632b22c73775806
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-08 12:19:26 +0900

    string.join이 궁금해서 해봄

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -13,12 +13,12 @@ namespace csloop
             long start = DateTime.Now.Ticks;
             long count = 0;
 
-            while(start+(10000000)>DateTime.Now.Ticks)
-            {
-                count++;
-            }
+            //while(start+(10000000)>DateTime.Now.Ticks)
+            //{
+            //    count++;
+            //}
 
-            Console.WriteLine(count + "만큼 반복했습니다.");
+            //Console.WriteLine(count + "만큼 반복했습니다.");
 
 
             string[] array = { "사과", "배", "포도", "딸기", "바나나" };
@@ -30,6 +30,11 @@ namespace csloop
             string input = "day6 comeback after 3days~!~!!!";
             Console.WriteLine(input.ToUpper());
             Console.WriteLine(input.ToLower());
+
+
+            string[] mem = { "박씨","김씨","강씨","윤씨"};
+            Console.WriteLine(string.Join(",", mem));
+
         }
     }
 }
