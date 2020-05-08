340c2bc6489b128b319695d4f6ab6e5d5f00bd4f
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-08 12:22:33 +0900

    지렁이 움직이기 예...뭐... 움직이네요~

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace csloop
@@ -34,6 +36,24 @@ namespace csloop
 
             string[] mem = { "박씨","김씨","강씨","윤씨"};
             Console.WriteLine(string.Join(",", mem));
+
+            int x = 1;
+            while (x < 50)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+
+
+                Thread.Sleep(100);
+                x++;
+            }            
 
         }
     }
