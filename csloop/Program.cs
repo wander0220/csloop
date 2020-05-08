ef21bf0535f15cb9af7bea1df11b56aac82ddb1a
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-08 12:00:37 +0900

    DateTime.Now.Ticks를 사용하여 성틍테스트가능

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -10,6 +10,15 @@ namespace csloop
     {
         static void Main(string[] args)
         {
+            long start = DateTime.Now.Ticks;
+            long count = 0;
+
+            while(start+(10000000)>DateTime.Now.Ticks)
+            {
+                count++;
+            }
+
+            Console.WriteLine(count + "만큼 반복했습니다.");
         }
     }
 }
